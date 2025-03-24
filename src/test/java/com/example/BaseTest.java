0fc6efd886d61c0b5747a875f43a81342a020c66
Yegor <kryvenkoyegor@gmail.com>
2025-03-24 21:31:52 +0200

    Added autotest for form submission

--- src/test/java/com/example/BaseTest.java
+++ src/test/java/com/example/BaseTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.BeforeMethod;
 public abstract class BaseTest {
     protected WebDriver driver;
     @BeforeMethod
-    public void setup() {
+    public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
