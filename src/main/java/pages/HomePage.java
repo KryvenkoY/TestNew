ce6618473acafeb4013b0aeab3ac93978083ac40
Yegor <kryvenkoyegor@gmail.com>
2025-03-27 01:04:30 +0200

    Added HomePage.java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,8 +1,11 @@
-package automationexercise;
+package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePage {
     private WebDriver driver;
